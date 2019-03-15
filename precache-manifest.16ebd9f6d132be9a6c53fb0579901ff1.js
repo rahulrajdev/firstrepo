d0bbe4e871ddbbcb3bd1984a6b3959a8a2808b36
Rahul Raj <rahulrajshopping@icloud.com>
2019-03-15 12:33:26 -0400

    Updates

---
+++ precache-manifest.16ebd9f6d132be9a6c53fb0579901ff1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b62309af7e76175547d6",
+    "url": "/memorygame/static/js/runtime~main.d170bb03.js"
+  },
+  {
+    "revision": "dede884c8541ddabe26f",
+    "url": "/memorygame/static/js/main.b4d8081d.chunk.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "/memorygame/static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "dede884c8541ddabe26f",
+    "url": "/memorygame/static/css/main.46b3d9db.chunk.css"
+  },
+  {
+    "revision": "823386668e593577618345213bbb73c9",
+    "url": "/memorygame/index.html"
+  }
+];
