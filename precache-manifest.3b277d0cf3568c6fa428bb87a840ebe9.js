4b1eb0f67d589c35bfa8e701c767ffe093e308d6
Rahul Raj <rahulrajshopping@icloud.com>
2019-03-18 20:58:41 -0400

    Updates

---
+++ precache-manifest.3b277d0cf3568c6fa428bb87a840ebe9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d83c22dcc2611df7753",
+    "url": "/Memory-Game/static/js/runtime~main.81d8dd5c.js"
+  },
+  {
+    "revision": "4c84d4460397b0c36232",
+    "url": "/Memory-Game/static/js/main.a1d9347e.chunk.js"
+  },
+  {
+    "revision": "81c621fba0d7771112a4",
+    "url": "/Memory-Game/static/js/2.e0afb958.chunk.js"
+  },
+  {
+    "revision": "4c84d4460397b0c36232",
+    "url": "/Memory-Game/static/css/main.308210ba.chunk.css"
+  },
+  {
+    "revision": "2fb6c9ea613491bc1021134cfb29c1f2",
+    "url": "/Memory-Game/index.html"
+  }
+];
